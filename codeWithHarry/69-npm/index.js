db6e488043f07b4c1d78fca8aed7c14a7948125a
subas <subasmohanty256@gmail.com>
2023-11-13 23:36:44 +0530

    69 npm done

---
+++ codeWithHarry/69-npm/index.js
@@ -0,0 +1,8 @@
+console.log("hello")
+
+// npm is a package manager
+// npm install <package name> : To install a package
+// npm i <package name> : works the same 
+// npm uninstall <package name> : To uninstall a package
+// npm install <package> --global : install the package globally
+// version: express 2.4.5 , major.minor.patch
